b182ebe76c6ef4f31e3beea11e7cbc50c986539f
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-07-26 03:20:27 +0600

    Dastan Tulokulov HW(3.1).(3.2)

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //String[] name = {"Dastan Tulokulov", "Cristiano Ronaldo", "Mark Zuckerberg"};
+        String[] name = new String[3];
+        name[0] = "Dastan Tulokulov";
+        name[1] = "Cristiano Ronaldo";
+        name[2] = "Mark Zuckerberg";
+
+        String[] names = new String[name.length + 1];
+        for (int i = 0; i < name.length; i++) {
+            names[i] = name[i];
+        }
+        names[name.length] = "Elon Musk";
+
+        System.out.println("Привет мой друг!");
+        System.out.println(".............................................");
+        System.out.println("Сегодня мы будем желать доброго времени суток нашим друзьям, имена которых мы уже указали в нашем массиве!");
+        System.out.println(".............................................");
+
+
+        switch (names[3]) {
+            case "Dastan Tulokulov":
+                System.out.println("Good morning " + names[0] + "!");
+                break;
+            case "Cristiano Ronaldo":
+                System.out.println("Good afternoon " + names[1] + "!");
+                break;
+            case "Mark Zuckerberg":
+                System.out.println("Good evening " + names[2] + "!");
+                break;
+            case "Elon Musk":
+                System.out.println("Good night " + names[3] + "!");
+                break;
+            default:
+                System.out.println("Please check the case correctness");
+                break;
+        }
+    }
+}
